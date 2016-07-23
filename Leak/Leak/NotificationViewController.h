9b525fb37fd23de5a397738eea767f093d028037
baoyanma <baoyan.ma@liuliu.co>
2016-07-23 21:29:44 +0800

    3.NSMutableArray

---
+++ Leak/Leak/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  Leak
+//
+//  Created by quanzhen on 16/7/23.
+//  Copyright © 2016年 baoyan. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface NotificationViewController : RootViewController
+
+@end
---
+++ Leak/Leak/NotificationViewController.m
@@ -0,0 +1,27 @@
+//
+//  NotificationViewController.m
+//  Leak
+//
+//  Created by quanzhen on 16/7/23.
+//  Copyright © 2016年 baoyan. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+
+@interface NotificationViewController ()
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
